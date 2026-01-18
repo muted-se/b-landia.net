10505c3b04487df9803ccf4f7b2b38fba3469aa9
Joakim Dellrud <joakim@dellrud.se>
2026-01-18 19:33:36 +0100

    feat(blob): minor changes

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 #!/bin/env bash
 
-podman login dhi.io
-
 podman run -it --rm \
        --entrypoint /bin/bash \
        --userns=keep-id \
@@ -9,3 +7,4 @@ podman run -it --rm \
        --user $(id -u):$(id -g) \
        --publish 1313:1313 \
        docker.io/chainguard/hugo:latest-dev@sha256:5c8916789231d4393600c933985fa03b65bf2f7d701af4ad7b75fc293eee44cb
+ 
